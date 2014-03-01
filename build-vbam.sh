1b1bfdda18a952d4750ce66370c24754aad517c3
Les Aker <me@lesaker.org>
2014-03-01 09:04:51 -0500

    these should probably be configurable/dynamic

--- build-vbam.sh
+++ build-vbam.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-export PATH=/Users/comex/Documents/emsdk_portable/emscripten/incoming/:$PATH
 set -xe
 rm -rf vbam-build
 mkdir vbam-build
 cd vbam-build
 touch ./-lc # lol
-cmake ../vbam -DENABLE_SDL=OFF -DENABLE_GTK=OFF -DENABLE_DEBUGGER=OFF -DENABLE_NLS=OFF -DENABLE_GBA_LOGGING=OFF -DCMAKE_CXX_FLAGS="-DJS -DNO_PNG" -DCMAKE_C_FLAGS="-DJS -DNO_PNG" -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DSDL_LIBRARY=-lc -DOPENGL_gl_LIBRARY=-lc -DOPENGL_glu_LIBRARY=-lc
+cmake ../vbam -DENABLE_SDL=OFF -DENABLE_GTK=OFF -DENABLE_DEBUGGER=OFF -DENABLE_NLS=OFF -DENABLE_GBA_LOGGING=OFF -DCMAKE_CXX_FLAGS="-DJS -DNO_PNG" -DCMAKE_C_FLAGS="-DJS -DNO_PNG" -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DSDL_LIBRARY=-lc -DOPENGL_gl_LIBRARY=-lc -DOPENGL_glu_LIBRARY=-lc -DSDL_INCLUDE_DIR=/usr/local/brew/include/
 exec ../rebuild-vbam.sh
